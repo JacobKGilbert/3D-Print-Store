dc213f40aa1e35451b1d5d36e0d8afdfe2fd9e30
JacobKGilbert <jacobkgilbert@protonmail.com>
2024-04-07 23:24:59 -0500

    testing fastify

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,14 @@
-const express = require('express')
-const app = express()
+const fastify = require('fastify')({logger: true})
 
-module.exports = {app}
+// Declare routes
+fastify.get('/', function handler (req, res) {
+  res.send({ hello: 'world'})
+})
+
+// Run Server
+fastify.listen({port:3000}, (err) => {
+  if (err) {
+    fastify.log.error(err)
+    process.exit(1)
+  }
+})
